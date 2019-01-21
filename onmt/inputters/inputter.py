7bf02511532b6759032a6fc62034b2549eae36e0
goncalomcorreia <goncalommac@gmail.com>
2019-01-21 12:45:52 +0000

    WIP: segments_ids

--- onmt/inputters/inputter.py
+++ onmt/inputters/inputter.py
@@ -109,8 +109,14 @@ def _feature_tokenize(
         tokens = bert_tokenizer.tokenize(string)
     else:
         tokens = string.split(tok_delim)
-    import ipdb
-    ipdb.set_trace()
+    if '[SEP]' in tokens:
+        src_len = len(' '.join(tokens).split(' [SEP] ')[0].split())
+        mt_len = len(' '.join(tokens).split(' [SEP] ')[1].split())
+        segments_ids = [0]*src_len + [1]*mt_len
+        tokens = ' '.join([' '.join(tokens).split(' [SEP] ')[0],
+                           ' '.join(tokens).split(' [SEP] ')[1]]).split()
+        import ipdb
+        ipdb.set_trace()
     if truncate is not None:
         tokens = tokens[:truncate]
     if feat_delim is not None:
